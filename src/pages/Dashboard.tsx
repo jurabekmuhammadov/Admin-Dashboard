d35820ec906a8ef6ed2b39de3d99e71f1e23f2dc
jurabekmuhammadov <jorabekmuhammadov7@gmail.com>
2024-05-03 02:15:58 +0500

    smth

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,7 +1,7 @@
 
 const Dashboard = () => {
     return (
-        <div>Dashboard</div>
+        <div>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolore placeat cupiditate vel deleniti. Tempora, expedita officia assumenda placeat minus laudantium porro voluptates incidunt error consequatur ullam harum non saepe distinctio quo aut possimus nostrum explicabo minima tempore impedit repellendus nihil maxime. Deserunt eos fugiat et ad dolores numquam modi eum, laudantium sunt facilis cum temporibus rem quasi non quas a? Eveniet enim, harum tempora voluptates quidem, ab corporis quasi qui pariatur voluptatem nisi accusamus ullam, veniam aut nulla? Dolorem odio facilis blanditiis iste, ullam officia ipsa assumenda, doloremque magnam obcaecati repellat voluptate, animi quos iusto non! Facilis, voluptatem architecto. Error.</div>
     )
 }
 
--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,68 +1,8 @@
-// import { Formik, Form, Field, ErrorMessage } from 'formik';
-// import * as Yup from 'yup';
-// import { useAuthStore } from '../app/auth/useAuthStore';
-
-// const SignInFormSchema = Yup.object().shape({
-//     email: Yup.string().email('Invalid email').required('Required'),
-//     password: Yup.string().required('Required').min(6, 'Password must be at least 6 characters'),
-//     confirmPassword: Yup.string().required('Required').oneOf([Yup.ref('password'), null], 'Passwords must match'),
-// });
-
-// const SignIn = () => {
-//     const signIn = useAuthStore(state => state.signIn);
-//     return (
-//         <div>
-//             <h1>Registration Form</h1>
-//             <Formik
-//                 initialValues={{
-//                     email: '',
-//                     full_name: '',
-//                     password: '',
-//                     confirmPassword: '',
-//                     phone_number: ''
-//                 }}
-//                 validationSchema={SignInFormSchema}
-//                 onSubmit={async (values, { resetForm }) => {
-//                     try {
-//                         const { confirmPassword, ...signInData } = values;
-//                         await signIn(JSON.stringify(signInData));
-//                         resetForm();
-//                     } catch (error) {
-//                         console.log(error.message);
-//                     }
-//                 }}
-//             >
-//                 {({ isSubmitting }) => (
-//                     <Form>
-//                         <div>
-//                             <label>Email</label>
-//                             <Field type="email" name="email" autoComplete="off" />
-//                             <ErrorMessage name="email" component="div" className="error" />
-//                         </div>
-//                         <div>
-//                             <label>Password</label>
-//                             <Field type="password" name="password" autoComplete="new-password" />
-//                             <ErrorMessage name="password" component="div" className="error" />
-//                         </div>
-//                         <div>
-//                             <label>Confirm Password</label>
-//                             <Field type="password" name="confirmPassword" autoComplete="new-password" />
-//                             <ErrorMessage name="confirmPassword" component="div" className="error" />
-//                         </div>
-//                         <button type="submit" disabled={isSubmitting}>Submit</button>
-//                     </Form>
-//                 )}
-//             </Formik>
-//         </div>
-//     );
-// };
-
-// export default SignIn;
-
-
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 import { useAuthStore } from '../app/auth/useAuthStore';
+import { useNavigate } from 'react-router-dom';
 
 const SignInFormSchema = Yup.object().shape({
     email: Yup.string().email('Invalid email').required('Required'),
@@ -70,11 +10,12 @@ const SignInFormSchema = Yup.object().shape({
 });
 
 const handleError = (error: any) => {
-    console.error('An error occurred:', error);
+    console.error('An error occurred:', error.message);
 };
 
 const SignIn = () => {
     const signIn = useAuthStore(state => state.signIn);
+    const navigete = useNavigate();
     return (
         <div>
             <h1>Sign In Form</h1>
@@ -88,6 +29,7 @@ const SignIn = () => {
                     try {
                         await signIn(values);
                         resetForm();
+                        navigete("/dashboard")
                     } catch (error) {
                         handleError(error);
                     }
